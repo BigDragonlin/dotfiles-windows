7d844d1d7c400dd2d7d8c8e4c0d61ee13973c30f
linmu <1363992060@qq.com>
2024-12-30 11:09:39 +0800

    一键安装软件

--- deps.ps1
+++ deps.ps1
@@ -8,23 +8,46 @@ if (!(Verify-Elevated)) {
    exit
 }
 
-# system and cli
-# install git
-winget install Git.Git                                   --silent --accept-package-agreements --accept-source-agreements --override "/VerySilent /NoRestart /o:PathOption=CmdTools /Components=""icons,assoc,assoc_sh,gitlfs"""
 
-# 安装nodejs
-winget install OpenJS.NodeJS                             --silent --accept-package-agreements --accept-source-agreements
-winget install Python.Python.3.12                        --silent --accept-package-agreements --accept-source-agreements
+# Function to check if a software is installed
+function Check-SoftwareInstalled {
+    param (
+        [string]$SoftwareName
+    )
+    # Use winget list to check if the software is already installed
+    $installed = winget list --name $SoftwareName 2>&1 | Out-String
+    return $installed -notmatch "No installed package found matching input criteria"
+}
 
-# browsers
-# 安装chrome
-winget install Google.Chrome                             --silent --accept-package-agreements --accept-source-agreements
+# Install Git
+if (-not (Check-SoftwareInstalled "Git")) {
+    winget install Git.Git --silent --accept-package-agreements --accept-source-agreements --override "/VerySilent /NoRestart /o:PathOption=CmdTools /Components=""icons,assoc,assoc_sh,gitlfs"""
+}
 
-# dev tools and frameworks
-# 安装poweshell
-winget install Microsoft.PowerShell                      --silent --accept-package-agreements --accept-source-agreements
-# 安装vim
-winget install Vim.Vim                                   --silent --accept-package-agreements --accept-source-agreements
+# Install Node.js
+if (-not (Check-SoftwareInstalled "Node.js")) {
+    winget install OpenJS.NodeJS --silent --accept-package-agreements --accept-source-agreements
+}
+
+# Install Python
+if (-not (Check-SoftwareInstalled "Python")) {
+    winget install Python.Python.3.12 --silent --accept-package-agreements --accept-source-agreements
+}
+
+# Install Google Chrome
+if (-not (Check-SoftwareInstalled "Google Chrome")) {
+    winget install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
+}
+
+# Install PowerShell
+if (-not (Check-SoftwareInstalled "PowerShell")) {
+    winget install Microsoft.PowerShell --silent --accept-package-agreements --accept-source-agreements
+}
+
+# Install Vim
+if (-not (Check-SoftwareInstalled "Vim")) {
+    winget install Vim.Vim --silent --accept-package-agreements --accept-source-agreements
+}
 
 Refresh-Environment
 
